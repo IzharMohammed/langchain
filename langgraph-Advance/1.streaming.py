0579ba944029a2d3520cc4889db23f9c32ffd62c
IzharMohammed <izharmohammed21@gmail.com>
2025-09-01 22:53:15 +0530

    Streaming

--- langgraph-Advance/1.streaming.py
+++ langgraph-Advance/1.streaming.py
@@ -15,8 +15,6 @@ class State(TypedDict):
     messages:Annotated[list,add_messages]
 
 
-
-
 llm_groq=ChatGroq(model="Llama3-8b-8192")
 print(llm_groq)
 rs = llm_groq.invoke("Hyy i am izhar, i like to solve real world problems using programming")
@@ -37,3 +35,22 @@ graph.add_node("superBot",superbot)
 ## Edges
 graph.add_edge(START,"superBot")
 graph.add_edge("superBot",END) 
+
+graph_builder = graph.compile(checkpointer=memory)
+
+## Invocation
+
+config={"configurable":{"thread_id":"1"}}
+
+graph_builder.invoke({"messages":"Hyy, i am izhar and i like to solve real world problems"},config)
+
+## Streaming the responses with stream method
+# Create a thread
+config={"configurable":{"thread_id":"2"}}
+
+for chunk in graph_builder.stream({"messages":"Hyy, i am izhar and i like to solve real world problems"},config,stream_mode="updates"):
+    print(chunk)
+
+for chunk in graph_builder.stream({"messages":"Hyy, i am izhar and i like to solve real world problems"},config,stream_mode="values"):
+    print(chunk)
+
